6b96fb3e50e91d7be716f480a097eade9d2f5fd3
Fabio.ROCCO <fabio.rocco@green-conseil.com>
2019-03-11 11:50:02 +0100

    CI: Integrates Circleci to the project

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 'use-strict';
 
 const express = require('express');
-// const Sentry = require('@sentry/node');
-const Raven = require('raven');
 
 const PORT = process.env.FRONT_HOST ? 80 : 8080;
 const HOST = process.env.FRONT_HOST ? '0.0.0.0' : 'localhost';
 const isAdmin = false;
-
-// Sentry.init({ dsn: 'https://362d3c3c1d564bbeaec599ba0c011947@sentry.io/1412297' });
-
-Raven.config('https://362d3c3c1d564bbeaec599ba0c011947@sentry.io/1412297', {
-    captureUnhandledRejections: true
-}).install();
-
 
 const app = express();
 app.get('/', (req, res) => {
