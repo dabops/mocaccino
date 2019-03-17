4cfda024e297046407d3ddf4ac45a01fab7e8b91
Fabio.ROCCO <fabio.rocco@green-conseil.com>
2019-03-17 18:17:58 +0100

    CI: Set the configuration to CI

--- test/TicTacToe.spec.js
+++ test/TicTacToe.spec.js
@@ -6,10 +6,6 @@ describe('TicTacToe', () => {
     const wrapper = mount(TicTacToe)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
-  test('renders correctly', () => {
-    const wrapper = mount(TicTacToe)
-    expect(wrapper.element).toMatchSnapshot()
-  })
   test('verify that the game is running', () => {
     const wrapper = mount(TicTacToe)
     wrapper.vm.onCell('1')
